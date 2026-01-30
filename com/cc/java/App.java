d2717e839a37cdbcf69132a3423131c610206229
Arya-Systems-Ops <aryakhalighi@gmail.com>
2026-01-30 10:43:59 +0100

    Add Iteration 2 and 3: Overloading and true Polymorphism

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,26 +3,33 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
         Queen queen = new Queen();
         Worker worker = new Worker();
-        Drone drone = new Drone();   
+        Drone drone = new Drone();
 
-        /* 1. Iteration  */
-
+        /* 1. Iteration - direkt */
         output(queen.doYourJob());
         output(queen.fly());
         output("-------------------");
-
-        output(worker.doYourJob());
-        output(worker.fly());
-        output("-------------------");
-
-        output(drone.doYourJob());
-        output(drone.fly());
-        output("-------------------");  
+        
+        /* 2. Iteration - Überladung */
+        // pollObj(queen);
+        // pollObj(worker);
+        // pollObj(drone);
+        
+        /* 3. Iteration - TRUE Polymorphismus */
+        testPolymorphism(queen);
+        testPolymorphism(worker);
+        testPolymorphism(drone);
     }
-
+    
+    // Iteration 3: eine Methode für alle!
+    private static void testPolymorphism(HoneyBee bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+        output("-------------------");
+    }
+    
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
